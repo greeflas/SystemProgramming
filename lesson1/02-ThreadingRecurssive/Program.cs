5bd1510a2c699a11d6308d85018f22743a1424b8
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-10-27 10:18:43 +0300

    Threading recurssive

---
+++ lesson1/02-ThreadingRecurssive/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_ThreadingRecurssive
+{
+    class Program
+    {
+        static int counter = 0;
+
+        static void Increment()
+        {
+            int id = Thread.CurrentThread.ManagedThreadId;
+
+            Console.WriteLine("Start thread id: {0}", id);
+
+            if(counter < 100)
+            {
+                counter++;
+                var thread = new Thread(Increment);
+                
+                thread.Start();
+                thread.Join();
+            }
+
+            Console.WriteLine("Finish: {0}", id);
+        }
+
+        static void Main(string[] args)
+        {
+            var thread = new Thread(Increment);
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
