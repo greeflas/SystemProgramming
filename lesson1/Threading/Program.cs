b87f0dfb93d7d27bce26e089fcae66fb7a4c247e
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-10-27 10:06:49 +0300

    Threading

---
+++ lesson1/Threading/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace _01_Threading
+{
+    class Program
+    {
+        static void ThreadFunc()
+        {
+            int thread_id = Thread.CurrentThread.ManagedThreadId;
+
+            Console.WriteLine("Start: {0}\n", thread_id);
+
+            for (int i = 0; i < 160; i++)
+            {
+                Thread.Sleep(20);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(". ");
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("\nFinish: {0}", thread_id);
+        }
+
+        static void Main(string[] args)
+        {
+            var thread = new Thread(new ThreadStart(ThreadFunc));
+
+            int thread_id = Thread.CurrentThread.ManagedThreadId;
+
+            thread.Start();
+            Console.WriteLine("Start: {0}\n", thread_id);
+
+            for (int i = 0; i < 160; i++)
+            {
+                Thread.Sleep(20);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(". ");
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("\nFinish: {0}", thread_id);
+        }
+    }
+}
