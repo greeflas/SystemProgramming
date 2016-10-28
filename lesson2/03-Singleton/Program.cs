0ae31e625e4f85b99a7ca8c1948bcc71994a75c7
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-10-28 10:16:05 +0300

    Lock stream

---
+++ lesson2/03-Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace _03_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Console.WriteLine(s1.GetHashCode());
+
+            Singleton s2 = Singleton.GetInstance();
+            Console.WriteLine(s2.GetHashCode());
+        }
+    }
+}
---
+++ lesson2/03-Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03_Singleton
+{
+    class Singleton
+    {
+        static Singleton instance;
+        static object lockObj = new object();
+
+        Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(instance == null)
+            {
+                lock (lockObj)
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+                }
+            }
+
+            return instance;
+        }
+    }
+}
