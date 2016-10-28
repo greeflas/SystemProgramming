e2734df2550465048d5056f65daace1e03156d85
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-10-28 09:38:41 +0300

    Timer

---
+++ lesson2/01-Timer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;   
+using System.Threading;
+
+namespace _01_Timer
+{
+    class Program
+    {
+        static int count = 0;
+
+        static void Tick(object obj)
+        {
+            Timer t = obj as Timer;
+
+            if (count < 10)
+            { 
+                count++;
+                Console.WriteLine("Timer work!");
+            }
+            else
+                t.Dispose();
+        }
+
+        static void Main(string[] args)
+        {
+            TimerCallback timertCallback = new TimerCallback(Tick);
+            Timer timer = new Timer(timertCallback);
+            timer.Change(1000, 1000);
+
+            Console.ReadKey();
+        }
+    }
+}
