f9325dfdcda74035487a304a56a396241ab3d8c2
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-10-28 09:49:44 +0300

    Thread Suspend/Resume

---
+++ lesson2/02-ThreadSuspendResume/MyThread.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_ThreadSuspendResume
+{
+    class MyThread
+    {
+        public Thread thread;
+
+        public MyThread()
+        {
+            thread = new Thread(new ThreadStart(Run));
+            thread.Start();
+        }
+
+        void Run()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < 70; i++)
+            {
+                Console.Write(". ");
+                Thread.Sleep(200);
+            }
+            Console.ResetColor();
+        }
+    }
+}
---
+++ lesson2/02-ThreadSuspendResume/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_ThreadSuspendResume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyThread my_thread = new MyThread();
+
+            Thread.Sleep(1000);
+            my_thread.thread.Suspend();
+
+            Console.WriteLine("\nMain thread");
+
+            my_thread.thread.Resume();
+        }
+    }
+}
