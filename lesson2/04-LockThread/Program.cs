5bd7c6205374572bd244539bbfa2f75427586257
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-01 09:13:30 +0200

    Lock thread

---
+++ lesson2/04-LockThread/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace _04_LockThread
+{
+    class Program
+    {
+        static object lockObj = new object();
+        static ReaderWriterLockSlim readWriteLock = new ReaderWriterLockSlim();
+
+        static void NotEven()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    lock(lockObj)
+                    {
+                        readWriteLock.EnterWriteLock();
+
+                        StreamWriter sw = new StreamWriter("../../data/test.txt", true);
+                        sw.WriteLine(i);
+                        sw.Close();
+
+                        readWriteLock.ExitWriteLock();
+
+                        Console.WriteLine("{0} saved {0}", Thread.CurrentThread.GetHashCode(), i);
+                    }
+                }
+                Thread.Sleep(300);
+            }
+        }
+
+        static void Even()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    lock (lockObj)
+                    {
+                        readWriteLock.EnterWriteLock();
+
+                        StreamWriter sw = new StreamWriter("../../data/test.txt", true);
+                        sw.WriteLine(i);
+                        sw.Close();
+
+                        readWriteLock.ExitWriteLock();
+
+                        Console.WriteLine("{0} saved {0}", Thread.CurrentThread.GetHashCode(), i);
+                    }
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if(!File.Exists("../../data/test.txt"))
+            {
+                File.Create("../../data/test.txt");
+            }
+
+            Thread th1 = new Thread(new ThreadStart(NotEven));
+            th1.Start();
+
+            Thread th2 = new Thread(new ThreadStart(Even));
+            th2.Start();
+        }
+    }
+}
