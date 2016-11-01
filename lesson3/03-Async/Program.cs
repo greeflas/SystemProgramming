56afa1008033e838e29a6c6141698be077fca3da
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-01 11:46:44 +0200

    Async

---
+++ lesson3/03-Async/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+
+namespace _03_Async
+{
+    class Program
+    {
+        delegate UInt64 AsyncSumDel(UInt64 n);
+
+        static UInt64 Sum(UInt64 n)
+        {
+            UInt64 sum = 1;
+            for (UInt64 i = 0; i < n; i++)
+                sum += i;
+
+            Thread.Sleep(3000);
+
+            return sum;
+        }
+
+        static void Method(IAsyncResult ar)
+        {
+            AsyncResult res = (AsyncResult)ar;
+            AsyncSumDel result = (AsyncSumDel)res.AsyncDelegate;
+
+            Console.WriteLine("Result: {0}", result.EndInvoke(ar));
+        }
+
+        static void Main(string[] args)
+        {
+            AsyncSumDel del = Sum;
+
+            IAsyncResult ar = del.BeginInvoke(1000000, Method, null);
+
+            Console.ReadKey();
+            
+            //Console.WriteLine("Start method...");
+            //UInt64 res = del.Invoke(1000000);
+            //Console.WriteLine("Result: {0}", res);
+        }
+    }
+}
