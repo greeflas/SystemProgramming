a0c5fcbf50a571934e4d6c87511d913b5d98044a
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-01 10:57:54 +0200

    Thread info

---
+++ lesson3/02-ThreadPool/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_ThreadPool
+{
+    class Program
+    {
+        static void ShowThreadInfo()
+        {
+            int avalibleWorkThreads;
+            int avalibleIOThreads;
+            int maxWorkThreads;
+            int minIOThreads;
+
+            ThreadPool.GetAvailableThreads(out avalibleWorkThreads, out avalibleIOThreads);
+            ThreadPool.GetMaxThreads(out maxWorkThreads, out minIOThreads);
+
+            Console.WriteLine("Work threads: {0}", avalibleWorkThreads);
+            Console.WriteLine("IO threads: {0}", avalibleIOThreads);
+            Console.WriteLine("Max work threads: {0}", maxWorkThreads);
+            Console.WriteLine("Max IO threads: {0}", minIOThreads);
+        }
+
+        static void Main(string[] args)
+        {
+            ShowThreadInfo();
+        }
+    }
+}
