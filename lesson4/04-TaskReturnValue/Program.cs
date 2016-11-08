5227c8a5a18047c99dcf6f4fe4d3026e64d0ae81
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-08 12:03:16 +0200

    Task: return value from task

---
+++ lesson4/04-TaskReturnValue/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _04_TaskReturnValue
+{
+    class Program
+    {
+        static string[] Firstnames = new string[]
+        {
+            "Vasya",
+            "Petro",
+            "Elizaveta"
+        };
+
+        static string[] Lastnames = new string[]
+        {
+            "Pupkin",
+            "Nikolaev",
+            "Ahvetov"
+        };
+
+        static List<Student> GetStudentList()
+        {
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(0, 2);
+
+                string firstname = Firstnames[index];
+                string lastname = Lastnames[index];
+
+                students.Add(new Student(firstname, lastname, r.Next(1, 12)));
+                Thread.Sleep(200);
+            }
+
+            return students;
+        }
+
+        static void Main(string[] args)
+        {
+            Task< List<Student> > task = new Task< List<Student> >(GetStudentList);
+            task.Start();
+            List<Student> students = task.Result;
+
+            foreach(Student s in students)
+            {
+                Console.WriteLine(s);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ lesson4/04-TaskReturnValue/Student.cs
@@ -0,0 +1,21 @@
+﻿namespace _04_TaskReturnValue
+{
+    class Student
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public double Rate { get; set; }
+
+        public Student(string firstname, string lastname, double rate)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Rate = rate;
+        }
+
+        public override string ToString()
+        {
+            return $"Firstname: {Firstname} \nLastname: {Lastname} \nRate: {Rate}";
+        }
+    }
+}
