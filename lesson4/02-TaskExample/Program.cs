b5ac25310c1cfaf9f1e0ed0d2f9a1409c9b7fb5c
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-08 09:59:05 +0200

    Task: Wait

---
+++ lesson4/02-TaskExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _02_TaskExample
+{
+    class Program
+    {
+        static void ShowNumber(object length)
+        {
+            int max = (int)length;
+
+            for (int i = 0; i < max; i++)
+            {
+                if((i % 21 == 0) || (i % 99 == 0))
+                    Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task task = new Task(ShowNumber, 50000);
+            task.Start();
+
+            task.Wait();
+            //Task.WaitAll(task);
+        }
+    }
+}
