8bc1c4168c88ae4630e38b5a009d3d8b16b6ba29
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-08 10:59:18 +0200

    Private24 API example

---
+++ lesson4/03-Private24/Private24.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Privat24API
+{
+    public class ExchangeRate
+    {
+        public string baseCurrency { get; set; }
+        public string currency { get; set; }
+        public decimal saleRateNB { get; set; }
+        public decimal purchaseRateNB { get; set; }
+        public decimal saleRate { get; set; }
+        public decimal purchaseRate { get; set; }
+    }
+
+    public class UserServices
+    {
+        public DateTime date { get; set; }
+        public string bank { get; set; }
+        public int baseCurrency { get; set; }
+        public string baseCurrencyLit { get; set; }
+
+        public List<ExchangeRate> exchangeRate { get; set; }
+    }
+}
---
+++ lesson4/03-Private24/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+using Privat24API;
+
+namespace _03_Private24
+{
+    class Program
+    {
+        public static void GetCurrency(object date)
+        {
+            DateTime d = (DateTime)date;
+            while (d < DateTime.Now)
+            {
+                string url = "https://api.privatbank.ua/p24api/exchange_rates?json&date=" + d.ToShortDateString();
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+                string answer = string.Empty;
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        answer = reader.ReadToEnd();
+                    }
+                }
+
+                var servicedata = JsonConvert
+                    .DeserializeObject<UserServices>(answer, new IsoDateTimeConverter { DateTimeFormat = "dd.MM.yyyy" });
+
+                foreach (var item in servicedata.exchangeRate)
+                {
+                    if (item.currency == "USD")
+                    {
+                        string Date = d.ToShortDateString();
+                        decimal USD = item.purchaseRate;
+
+                        Console.WriteLine($"Date: {Date}");
+                        Console.WriteLine($"USD: {USD:f}");
+                        Console.WriteLine();
+                    }
+                }
+
+                Thread.Sleep(1000);
+                d = d.AddDays(1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task task = new Task(GetCurrency, new DateTime(2016, 10, 1));
+            task.Start();
+            task.Wait();
+        }
+    }
+}
