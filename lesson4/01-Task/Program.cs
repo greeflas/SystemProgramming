80f8642f3fa774d7226f199771395b387199a7ae
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-08 09:43:05 +0200

    Task

---
+++ lesson4/01-Task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01_Task
+{
+    class Program
+    {
+        static void Method()
+        {
+            for (int i = 0; true; i++)
+            {
+                Console.Write("* ");
+                Thread.Sleep(200);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Action action = new Action(Method);
+            Task task = new Task(action);
+            task.Start();
+
+            for (int i = 0; i < 40; i++)
+            {
+                Console.Write(". ");
+                Thread.Sleep(200);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
