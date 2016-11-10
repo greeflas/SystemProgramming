3e5ef6f387815fe1a5e129fa2c6e972168730f14
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-10 11:20:09 +0200

    IDisposable

---
+++ lesson5/04-IDisposable/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _04_IDisposable
+{
+    class ResourceWrapper : IDisposable
+    {
+        bool disposed = false;
+
+        ~ResourceWrapper()
+        {
+            CleanUp(false);
+        }
+
+        public void Dispose()
+        {
+            CleanUp(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void CleanUp(bool clean)
+        {
+            if(!disposed)
+            {
+                if(clean)
+                {
+                    Console.WriteLine("Resourse cleaning...");
+
+                }
+
+                Console.WriteLine("Finalized!");
+            }
+
+            disposed = true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ResourceWrapper wrapp = new ResourceWrapper())
+            {
+                Console.WriteLine(wrapp);
+            }
+
+            ResourceWrapper wrapp2 = new ResourceWrapper();
+            Console.WriteLine(wrapp2);
+
+            wrapp2.Dispose();
+            wrapp2.Dispose();
+            wrapp2.Dispose();
+            wrapp2.Dispose();
+
+            ResourceWrapper wrapp3 = new ResourceWrapper();
+            Console.ReadKey();
+        }
+    }
+}
