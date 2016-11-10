b1ad4f9bb5cf699cddf9fa9bfb630a2a64355d05
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-10 11:19:51 +0200

    Finalizer

---
+++ lesson5/03-Finalizer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03_Finalizer
+{
+    class Finalizer
+    {
+        ~Finalizer()
+        {
+            for (int i = 0; i < 1000; i++)
+                Console.Write(". ");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Finalizer f = new Finalizer();
+            GC.Collect(); // Release
+            // f = null; // Debug
+
+            Console.ReadKey();
+        }
+    }
+}
