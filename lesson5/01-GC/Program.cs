dca9cad2622ad388e45779570a918761481fc17b
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-10 11:19:30 +0200

    Garbage Collector

---
+++ lesson5/01-GC/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01_GC
+{
+    class Program
+    {
+        class Test
+        {
+            int[] arr = new int[100000000];
+
+            public void Method(int i)
+            {
+                Console.WriteLine(i);
+            }
+
+            ~Test()
+            {
+                Console.WriteLine("Object {0} has been removed!", this.GetHashCode());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Test[] arr = new Test[1000];
+
+            try
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = new Test();
+                    arr[i].Method(i);
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
---
+++ lesson5/02-WorkWithGarabageCollector/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_WorkWithGarabageCollector
+{
+    class Person
+    {
+        string firstname;
+        string lastname;
+        uint age;
+
+        public Person()
+        {
+            firstname = "";
+            lastname = "";
+            age = 0;
+        }
+
+        public void Init(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Person p = new Person();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start memory: {0}", GC.GetTotalMemory(false));
+
+            Person obj = new Person();
+            Console.WriteLine("Object generation: {0}", GC.GetGeneration(obj));
+            Console.WriteLine("Memory after object creation: {0}", GC.GetTotalMemory(false));
+
+            obj.Init(1000);
+            Console.WriteLine("Object generation after creation trash: {0}", GC.GetGeneration(obj));
+            Console.WriteLine("Memory after creation trash: {0}", GC.GetTotalMemory(false));
+
+            GC.Collect();
+            Console.WriteLine("Object generation after collect: {0}", GC.GetGeneration(obj));
+            Console.WriteLine("Memory after collect: {0}", GC.GetTotalMemory(false));
+        }
+    }
+}
