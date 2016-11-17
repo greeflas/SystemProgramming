ac3217ff3bcc580d188ab1ad67d10582a4b8cc07
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-17 10:06:34 +0200

    Reflaction: class Type

---
+++ lesson6/01-Reflection/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reflection;
+
+namespace LessonReflection
+{
+    class Program
+    {
+        static void DisplayClassInfo(Type t)
+        {
+            Console.WriteLine("Name: {0}", t.Name);
+            Console.WriteLine("Base type: {0}", t.BaseType);
+        }
+
+        static void DisplayMethods(Type t)
+        {
+            MethodInfo[] methods = t.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method);
+            }
+        }
+
+        static void DisplayFields(Type t)
+        {
+            FieldInfo[] fields = t.GetFields();
+            foreach (FieldInfo field in fields)
+            {
+                Console.WriteLine(field);
+            }
+        }
+
+        static void DisplayProperties(Type t)
+        {
+            PropertyInfo[] properties = t.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var MyType = new TestType();
+
+            /* Getting type */
+            // 1
+            Type t = MyType.GetType();
+            Console.WriteLine(t);
+
+            // 2
+            t = Type.GetType("LessonReflection.TestType");
+            Console.WriteLine(t);
+
+            /* Methods of class Type */
+            Console.WriteLine("\n\tClass info");
+            DisplayClassInfo(t);
+
+            Console.WriteLine("\n\tMethods");
+            DisplayMethods(t);
+
+            Console.WriteLine("\n\tFields");
+            DisplayFields(t);
+
+            Console.WriteLine("\n\tProperties");
+            DisplayProperties(t);
+        }
+    }
+}
---
+++ lesson6/01-Reflection/TestType.cs
@@ -0,0 +1,20 @@
+﻿namespace LessonReflection
+{
+    class TestType
+    {
+        int res;
+        public int a;
+        public int b;
+
+        public int Res
+        {
+            get { return res; }
+            set { res = value; }
+        }
+
+        public int Calc()
+        {
+            return a + b;
+        }
+    }
+}
