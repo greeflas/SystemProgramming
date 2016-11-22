46f37d84cbe37594acc3a79d54c4828850818f4d
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-22 12:02:55 +0200

    Domains

---
+++ lesson7/04-ApplicationDomain/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace _04_ApplicationDomain
+{
+    class Program
+    {
+        static void InitDomain()
+        {
+            AppDomain myDomain = AppDomain.CreateDomain("My domain");
+            myDomain.ExecuteAssembly(@"D:\Shop\Shop.WinForm.exe");
+
+            AppDomain.Unload(myDomain);
+        }
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(InitDomain));
+            thread.Start();
+
+            Console.WriteLine("Input path to assembly");
+            Console.Write("> ");
+            string path_to_assembly = Console.ReadLine();
+        }
+    }
+}
---
+++ lesson7/05-WinformsAppDomain/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace _05_WinformsAppDomain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Newthread();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void Newthread()
+        {
+            Thread thread = new Thread(new ThreadStart(InitDomain));
+            thread.Start();
+        }
+
+        void InitDomain()
+        {
+            AppDomain MyDomain = AppDomain.CreateDomain("My domain");
+            MyDomain.ExecuteAssembly(@"D:\Shop\Shop.WinForm.exe");
+            AppDomain.Unload(MyDomain);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Main domain is work!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Newthread();
+        }
+    }
+}
