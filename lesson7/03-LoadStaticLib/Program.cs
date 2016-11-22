69086939a5bb849e5b925510f014c2888bb61573
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-22 11:34:26 +0200

    Load static library

---
+++ lesson7/03-LoadStaticLib/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace _03_LoadStaticLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load lib and show classes
+            Assembly assembly = Assembly.Load(AssemblyName.GetAssemblyName("CarLibrary.dll"));
+
+            Module module = assembly.GetModule("CarLibrary.dll");
+
+            foreach(Type t in module.GetTypes())
+                Console.WriteLine(t.FullName);
+
+            // Call method from lib
+            Type type = assembly.GetType("CarLibrary.SecretCar");
+
+            object o = Activator.CreateInstance(type, new object[] { "Toyota", 150, 110 });
+            MethodInfo method = type.GetMethod("Acceleration");
+            method.Invoke(o, null);
+        }
+    }
+}
