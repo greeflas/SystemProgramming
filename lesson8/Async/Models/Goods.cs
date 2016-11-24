afb0e29beb1f1241e3cfaf51353c2ed5056afa85
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-24 11:51:54 +0200

    Async

---
+++ lesson8/Async/Models/Goods.cs
@@ -0,0 +1,17 @@
+﻿namespace App.Models
+{
+    class Goods
+    {
+        public int GoodId { get; set; }
+        public string GoodName { get; set; }
+        public string ManufacturerName { get; set; }
+        public string CategoryName { get; set; }
+        public decimal Price { get; set; }
+        public decimal GoodCount { get; set; }
+
+        public override string ToString()
+        {
+            return $"{GoodName}";
+        }
+    }
+}
---
+++ lesson8/Async/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using App.Entities;
+using App.Models;
+using System.Threading;
+
+namespace App
+{
+    class Program
+    {
+        static ShopContext context = new ShopContext();
+
+        static List<Goods> goods;
+
+        static List<Goods> DisplayGoods(int catId, int manId)
+        {
+            string query = $"EXEC dbo.GetGoods ";
+            query += (catId != -1) ? catId.ToString() : String.Empty;
+            query += (manId != -1) ? catId.ToString() : String.Empty;
+
+            List<Goods> goods = context.Database
+                .SqlQuery<Goods>($"EXEC dbo.GetGoods {catId}")
+                .ToList();
+
+            return goods;
+        }
+
+        static void DisplayResult()
+        {
+            foreach (Goods g in goods)
+                Console.WriteLine(g);
+        }
+
+        static void Main(string[] args)
+        {
+            var action = new Func<int, int, List<Goods>>(DisplayGoods);
+            IAsyncResult result = action.BeginInvoke(1, -1, null, null);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Main thread {0}", i);
+            }
+
+            goods = action.EndInvoke(result);
+
+            var action2 = new Action(DisplayResult);
+            IAsyncResult result2 = action2.BeginInvoke(null, null);
+
+            for (int i = 10; i < 20; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Main thread {0}", i);
+            }
+
+            action2.EndInvoke(result2);
+        }
+    }
+}
