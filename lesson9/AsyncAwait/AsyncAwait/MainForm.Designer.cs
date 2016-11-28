fc87fa47fe837a6b5104e52f25497ec3b0adee32
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-11-28 11:56:43 +0200

    Async await

---
+++ lesson9/AsyncAwait/AsyncAwait/MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace AsyncAwait
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbData = new System.Windows.Forms.TextBox();
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.lblData = new System.Windows.Forms.Label();
+            this.A = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.TextBox();
+            this.Res = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbData
+            // 
+            this.tbData.Location = new System.Drawing.Point(12, 12);
+            this.tbData.Name = "tbData";
+            this.tbData.Size = new System.Drawing.Size(260, 20);
+            this.tbData.TabIndex = 0;
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Location = new System.Drawing.Point(197, 58);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(75, 23);
+            this.btnGetData.TabIndex = 1;
+            this.btnGetData.Text = "Get data";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(12, 43);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(30, 13);
+            this.lblData.TabIndex = 2;
+            this.lblData.Text = "Data";
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(15, 153);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(51, 20);
+            this.A.TabIndex = 3;
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(82, 153);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(51, 20);
+            this.B.TabIndex = 4;
+            // 
+            // Res
+            // 
+            this.Res.Location = new System.Drawing.Point(160, 153);
+            this.Res.Name = "Res";
+            this.Res.Size = new System.Drawing.Size(112, 20);
+            this.Res.TabIndex = 5;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(197, 199);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 6;
+            this.btnCalc.Text = "Calc";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 234);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.Res);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.btnGetData);
+            this.Controls.Add(this.tbData);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbData;
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.TextBox A;
+        private System.Windows.Forms.TextBox B;
+        private System.Windows.Forms.TextBox Res;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
---
+++ lesson9/AsyncAwait/AsyncAwait/MainForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    public partial class MainForm : Form
+    {
+        delegate string MyDelegate();
+        delegate string CalcDel(string a, string b);
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        string GetData()
+        {
+            Thread.Sleep(5000);
+
+            return "Test data";
+        }
+
+        Task<string> GetData2()
+        {
+            return Task.Run(
+                () =>
+                {
+                    Thread.Sleep(5000);
+                    return "Test data 2";
+                }
+            );
+        }
+
+        Task<string> Sum(string a, string b)
+        {
+            return Task.Run(
+                () =>
+                {
+                    Thread.Sleep(5000);
+                    return (Convert.ToInt32(a) + Convert.ToInt32(b)).ToString();
+                }
+            );
+        }
+
+        string Sum2(string a, string b)
+        {
+            Thread.Sleep(5000);
+            return (Convert.ToInt32(a) + Convert.ToInt32(b)).ToString();
+        }
+
+        void Callback(IAsyncResult ar)
+        {
+            AsyncResult res = (AsyncResult)ar;
+            MyDelegate caller = (MyDelegate)res.AsyncDelegate;
+
+            lblData.Text = caller.EndInvoke(ar);
+        }
+
+        private async void btnGetData_Click(object sender, EventArgs e)
+        {
+            //MyDelegate del = GetData;
+            //IAsyncResult res = del.BeginInvoke(Callback, null);
+
+            lblData.Text = await GetData2();
+        }
+
+        void CalcCallback(IAsyncResult ar)
+        {
+            AsyncResult res = (AsyncResult)ar;
+            CalcDel caller = (CalcDel)res.AsyncDelegate;
+
+            Res.Text = caller.EndInvoke(ar);
+        }
+
+        private async void btnCalc_Click(object sender, EventArgs e)
+        {
+            //string a = A.Text;
+            //string b = B.Text;
+
+            //Res.Text = await Sum(a, b);
+
+            CalcDel del = Sum2;
+            IAsyncResult res = del.BeginInvoke(A.Text, B.Text, CalcCallback, null);
+        }
+    }
+}
